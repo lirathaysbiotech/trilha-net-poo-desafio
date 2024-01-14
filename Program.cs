87cc0c22d10a3ed1296791a9bc07d082f641773d
Thays Lira <thayslira@outlook.com.br>
2024-01-14 20:59:13 -0300

    Implementando mudanças propostas pelo desafio do DecolaTech

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Whatsapp");
+
+        Console.WriteLine("Smartphone iPhone:");
+        Smartphone iphone = new Iphone(numero: "987654", modelo: "Modelo 2", imei: "22222222", memoria: 128);
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Instagram");
+
+        Console.WriteLine("Testes concluídos com sucesso!");
+    }
+}
